689a2e5dd327f039f7dbb68668ebb98bb06ff005
BagusAri-17 <bagusari.widhi@gmail.com>
2023-09-21 00:25:16 +0800

    add task and resume about react hook

---
+++ 14_react-hook/praktikum/react-vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CreateProduct from './pages/CreateProduct'
+import CreateAccount from './pages/CreateAccount'
+
+function App() {
+
+  return (
+    <>
+      <CreateProduct />
+      {/* <CreateAccount /> */}
+    </>
+  )
+}
+
+export default App
---
+++ 14_react-hook/praktikum/react-vite-project/src/pages/CreateAccount.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../component/Header'
+import Form from '../component/createAccount/Form'
+import Table from '../component/createAccount/Table'
+
+const CreateAccount = () => {
+  return (
+    <>
+        <Header />
+        <Form />
+        <Table />
+    </>
+  )
+}
+
+export default CreateAccount
