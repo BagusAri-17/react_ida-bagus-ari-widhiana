5460c1ac6f4b8b0768b18814627bf7306786e986
BagusAri-17 <bagusari.widhi@gmail.com>
2023-09-21 07:37:02 +0800

    add task and resume about react hook

---
+++ 14_react-hook/praktikum/react-vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateProduct from './pages/CreateProduct'
+
+const App = () => {
+  return (
+    <>
+      <CreateProduct />
+    
+    </>
+  )
+}
+
+export default App
---
+++ 14_react-hook/praktikum/react-vite-project/src/pages/CreateProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/Header'
+import Home from '../component/CreateProduct/Home'
+
+const CreateProduct = () => {
+  return (
+    <>
+        <Header />
+        <Home />
+    </>
+  )
+}
+
+export default CreateProduct
